c02de15010f3128b8297e5d303101bffe6c954e7
Max Lincoln <max@devopsy.com>
2013-11-27 22:33:21 -0300

    ZoomableImage helper

---
+++ plugins/zoomable_image_tag.rb
@@ -0,0 +1,28 @@
+# Title: Simple Image tag for Jekyll
+# Authors: Brandon Mathis http://brandonmathis.com
+#          Felix Schäfer, Frederic Hemberger
+# Description: Easily output images with optional class names, width, height, title and alt attributes
+#
+# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
+#
+# Examples:
+# {% img /images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
+#
+# Output:
+# <img src="/images/ninja.png">
+# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
+# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
+#
+
+module Jekyll
+
+  class ZoomableImageTag < Jekyll::ImageTag
+    def render(context)
+      "<a href=\"#{@img['src']}\">#{super}</a>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('zoomable_img', Jekyll::ZoomableImageTag)
