d03e6dbbc146f6fdf6c00280b9c4395750dcfe56
Max Lincoln <max@devopsy.com>
2013-11-28 13:04:47 -0300

    fix documentation on ZoomableImage

--- plugins/zoomable_image_tag.rb
+++ plugins/zoomable_image_tag.rb
@@ -1,19 +1,18 @@
-# Title: Simple Image tag for Jekyll
-# Authors: Brandon Mathis http://brandonmathis.com
-#          Felix Schäfer, Frederic Hemberger
-# Description: Easily output images with optional class names, width, height, title and alt attributes
+# Title: Zoomable Image tag wrapper for Jekyll
+# Authors: Max Lincoln
+# Description: Just wraps a normal image tag, adding a link to view the full image.
 #
-# Syntax {% img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
+# Syntax {% zoomable_img [class name(s)] [http[s]:/]/path/to/image [width [height]] [title text | "title text" ["alt text"]] %}
 #
 # Examples:
-# {% img /images/ninja.png Ninja Attack! %}
-# {% img left half http://site.com/images/ninja.png Ninja Attack! %}
-# {% img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
+# {% zoomable_img /images/ninja.png Ninja Attack! %}
+# {% zoomable_img left half http://site.com/images/ninja.png Ninja Attack! %}
+# {% zoomable_img left half http://site.com/images/ninja.png 150 150 "Ninja Attack!" "Ninja in attack posture" %}
 #
 # Output:
-# <img src="/images/ninja.png">
-# <img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!">
-# <img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture">
+# <a href="/images/ninja.png""><img src="/images/ninja.png"></a>
+# <a href="http://site.com/images/ninja.png"><img class="left half" src="http://site.com/images/ninja.png" title="Ninja Attack!" alt="Ninja Attack!"></a>
+# <a href="http://site.com/images/ninja.png"><img class="left half" src="http://site.com/images/ninja.png" width="150" height="150" title="Ninja Attack!" alt="Ninja in attack posture"></a>
 #
 
 module Jekyll
